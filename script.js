0c567a6e612c8cfb08f3993329cca367500718ce
Shota <shotikotchelidze@gmail.com>
2023-11-28 18:38:28 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+
+// console.log(ageJonas,ageSarah)
+// console.log(now - 1991 > now - 2018);
+
+
+// let x, y;
+// x = y = 25 - 10 - 5;
+// console.log(x, y);
+
+// const averageAge = ageJonas + ageSarah / 2;
+// console.log(ageJonas, ageSarah, averageAge);
+
+
+const firstName = 'Jonas';
+const job = 'Teacher';
+const birthYear = 1991;
+const year = 2037;
+
+//  const jonas = "i'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
+const jonas = "i'm " + firstName + ', a ' + year - birthYear
+
+// + ' years old ' + job + '!';
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}`;
+console.log(jonasNew);
+
+console.log("abc" - 1);
